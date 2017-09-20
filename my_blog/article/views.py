a75f2240b6ef450bcb0fbbf8432a730cfc0d7b97
Juot <gongpeng9449@gmail.com>
2017-09-21 03:21:40 +0800

    2017年09月21日03:21:38

---
+++ my_blog/article/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+from django.http import Http404
+
+# Create your views here.
+def home(request):
+    post_list = Article.objects.all()  
+    return render(request, 'home.html', {'post_list' : post_list})
+
+def detail(request, my_args):
+    print(my_args)
+    post = Article.objects.get(id=int(my_args))
+    return render(request, 'post.html', {'post': post})
